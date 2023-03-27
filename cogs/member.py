630a22367f229ea84b2dda08f79ea4d55ae96fa3
Shalltear-ov <103678267+Shalltear-ov@users.noreply.github.com>
2023-03-27 09:40:55 +0300

    Update member.py

--- cogs/member.py
+++ cogs/member.py
@@ -24,7 +24,8 @@ async def get_role_shop_list(start, sort='count', reverse=False):
     components.append(Button(style=ButtonStyle.green, label=f"Назад", custom_id=f'shop {start - 3}', row=2))
     components.append(Button(style=ButtonStyle.green, label=f"Вперед", custom_id=f'shop {start + 3}', row=2))
     embed = EMBED.get_shop_embed(roles)
-    embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/882429179512123463/1b2e01e540d1451e4699615f0a885f7e.png?size=1024")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/882429179512123463/1b2e01e540d1451e4699615f0a885f7e"
+                            ".png?size=1024")
     return embed, components
 
 
@@ -51,47 +52,13 @@ class Member(commands.Cog):
     async def shop(self, ctx: disnake.MessageCommandInteraction):
         embed, components = await get_role_shop_list(0)
         await ctx.send(embed=embed, components=components)
-    #     roles = User(ctx.user.id).get_role_shop()
-    #     select_components = list()
-    #     for index, role in enumerate(roles, start=1):
-    #         select_components.append(Button(style=ButtonStyle.green, label=f"{index}", custom_id=f"buy {role[0]} {role[2]}", row=1))
-    #     select_components.append(Button(style=ButtonStyle.green, label="Назад", custom_id=f'shop 0', row=2))
-    #     select_components.append(Button(style=ButtonStyle.green, label="Вперед", custom_id=f'shop 5', row=2))
-    #     embed = EMBED.shop_embed(roles)
-    #     embed.set_thumbnail(ctx.user.avatar)
-    #     await ctx.response.send_message(embed=embed, components=select_components)
-    #     try:
-    #         await self.bot.wait_for(
-    #             "button_click", timeout=30, check=lambda message: message.author == ctx.author)
-    #     except asyncio.TimeoutError:
-    #         for component in select_components:
-    #             component.disabled = True
-    #         await ctx.edit_original_message(components=select_components)
-    #         return
-    #
+
     @commands.Cog.listener("on_button_click")
     async def button_click_member(self, inter: disnake.MessageInteraction):
         if inter.component.custom_id.startswith('shop'):
             start = int(inter.component.custom_id.split()[1])
             embed, components = await get_role_shop_list(start)
             await inter.response.edit_message(embed=embed, components=components)
-    #         roles = User(inter.author.id).get_role_shop(index_call_back)
-    #         select_components = list()
-    #         for index, role in enumerate(roles, start=index_call_back + 1):
-    #             select_components.append(Button(style=ButtonStyle.green, label=f"{index}", custom_id=f"buy {role[0]} {role[2]}", row=1))
-    #         select_components.append(Button(style=ButtonStyle.green, label="Назад", custom_id=f'shop {index_call_back - 5}', row=2))
-    #         select_components.append(Button(style=ButtonStyle.green, label="Вперед", custom_id=f'shop {index_call_back + 5}', row=2))
-    #         embed = EMBED.shop_embed(roles, index_call_back)
-    #         embed.set_thumbnail(inter.author.avatar)
-    #         await inter.response.edit_message(embed=embed, components=select_components)
-    #         try:
-    #             await self.bot.wait_for(
-    #                 "button_click", timeout=30, check=lambda message: message.author == inter.author)
-    #         except asyncio.TimeoutError:
-    #             for component in select_components:
-    #                 component.disabled = True
-    #             await inter.edit_original_message(components=select_components)
-    #             return
         if inter.component.custom_id.startswith('buy'):
             _, role_id, cost = inter.component.custom_id.split()
             embed = EMBED.get_buy_embed(inter.author.id, role_id, int(cost))
@@ -114,23 +81,6 @@ class Member(commands.Cog):
                     await inter.response.edit_message("ty", embed=None, components=None)
             else:
                 await inter.response.edit_message("Не хватает монет", embed=None, components=None)
-
-
-    #         role_id = int(role_id)
-    #         cost = int(cost)
-    #         user = User(inter.author.id)
-    #         if user.balance >= cost:
-    #             user.add_money(-cost)
-    #             role = inter.guild.get_role(role_id)
-    #             if role is not None:
-    #                 await inter.author.add_roles(role)
-    #                 await inter.response.edit_message("Роль успешно куплена", components=None, embed=None)
-    #             else:
-    #                 await inter.response.edit_message("Роль удалена", components=None, embed=None)
-    #         else:
-    #             await inter.response.edit_message("Где бабки", components=None, embed=None)
-
-
 
 
 
