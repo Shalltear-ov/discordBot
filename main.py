ffb6c442e280c13c63ce5ca356b5f100869e9084
Shalltear-ov <103678267+Shalltear-ov@users.noreply.github.com>
2023-03-30 21:57:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import disnake
 from Userform import SETTING
 from disnake.ext import commands
 import os
-
+from typing import Optional
 
 client = commands.Bot(command_prefix='!', intents=disnake.Intents.all())
 client.remove_command('help')
+cogs_list = []
 
 
 @client.slash_command(name='load', description='load cogs')
@@ -17,12 +18,24 @@ async def load(ctx: disnake.ApplicationCommandInteraction, extension):
     await ctx.edit_original_message("Cogs is loaded")
 
 
+@load.autocomplete("extension")
+async def autocomplete(self, string: str):
+    string = string.lower()
+    return [cog for cog in cogs_list if string in cog.lower()]
+
+
 @client.slash_command(name='unload', description='unload cogs')
 @commands.has_permissions(administrator=True)
 async def load(ctx: disnake.ApplicationCommandInteraction, extension):
     await ctx.send("wait...", ephemeral=True)
     client.unload_extension(f"cogs.{extension}")
     await ctx.edit_original_message("Cogs is unloaded")
+
+
+@load.autocomplete("extension")
+async def autocomplete(self, string: str):
+    string = string.lower()
+    return [cog for cog in cogs_list if string in cog.lower()]
 
 
 @client.slash_command(name='reload', description='reload cogs')
@@ -34,10 +47,26 @@ async def load(ctx: disnake.ApplicationCommandInteraction, extension):
     await ctx.edit_original_message("Cogs is reloaded")
 
 
+@load.autocomplete("extension")
+async def autocomplete(self, string: str):
+    string = string.lower()
+    return [cog for cog in cogs_list if string in cog.lower()]
+
+
+@client.slash_command(name='reload_all', description='reload cogs')
+@commands.has_permissions(administrator=True)
+async def load_all(ctx: disnake.ApplicationCommandInteraction):
+    await ctx.send("wait...", ephemeral=True)
+    for cog in cogs_list:
+        client.unload_extension(f"cogs.{cog}")
+        client.load_extension(f"cogs.{cog}")
+    await ctx.edit_original_message("Cogs is reloaded")
+
+
 for file in os.listdir("./cogs"):
     if file.endswith(".py"):
+        cogs_list.append(file[:-3])
         client.load_extension(f"cogs.{file[:-3]}")
-
 
 if __name__ == '__main__':
     client.run(SETTING['token'])
